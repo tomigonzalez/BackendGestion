7e70a254cb45452ccea13ed56188fd220fc8efb5
Tomas Gonzalez <tomimateo2008@hotmail.com>
2024-10-11 23:02:46 -0300

    actualizacion

--- src/db.js
+++ src/db.js
@@ -1,5 +1,11 @@
 import pg from "pg";
-import { DB_DATABASE, DB_HOST, DB_PASSWORD, DB_PORT, DB_USER } from "./config";
+import {
+  DB_DATABASE,
+  DB_HOST,
+  DB_PASSWORD,
+  DB_PORT,
+  DB_USER,
+} from "./config.js";
 
 export const pool = new pg.Pool({
   user: DB_USER,
