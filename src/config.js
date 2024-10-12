6e7423f9933bb1bb35c77deb459615b86aac2ff8
Tomas Gonzalez <tomimateo2008@hotmail.com>
2024-10-11 23:21:53 -0300

    actualizacion

--- src/config.js
+++ src/config.js
@@ -4,4 +4,4 @@ export const DB_PASSWORD = process.env.DB_PASSWORD;
 export const DB_DATABASE = process.env.DB_DATABASE;
 export const DB_PORT = process.env.DB_PORT;
 
-export const PORT = process.env.PORT || 3000;
+export const PORT = process.env.DB_PORT || 3000;
